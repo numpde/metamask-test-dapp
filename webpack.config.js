5b3d190c29c9330b063383c3abeb03a683edc1a8
RA <4658911-numpde@users.noreply.gitlab.com>
2022-06-27 11:37:32 +0300

    flatten: false

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          flatten: true,
+          flatten: false,
           from: './src/*',
           globOptions: {
             ignore: ['**/*.js'],
